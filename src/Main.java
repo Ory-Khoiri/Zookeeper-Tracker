86f86e497130e0a4dfd584e9bb8a1497b4170366
Ory-Khoiri <25oryk@gmail.com>
2023-01-31 07:45:46 +0000

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        camel();
     }
+
+
+    static void camel(){
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+        System.out.println(camel);
+
+
+
+    }
+
+
 }
